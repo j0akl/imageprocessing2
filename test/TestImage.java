f8eaa5599f47d408d1282e9ea387a94318662a16
Maya Janicki <mayajanicki@Mayas-MacBook-Pro-2.local>
2021-06-11 15:29:55 -0400

    final?

--- test/TestImage.java
+++ test/TestImage.java
@@ -1,10 +1,17 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 import model.image.BasicImage;
 import model.image.Image;
+import model.operation.Blur;
+import model.operation.Operation;
+import model.operation.OperationFactory;
+import model.operation.Sepia;
 import model.pixel.Pixel;
 import org.junit.Before;
 import org.junit.Test;
@@ -36,6 +43,43 @@ public class TestImage {
         }
       }
     }
+  }
+
+  @Test
+  public void testSave() throws IOException {
+    setup();
+    this.checkerboard.save();
+  }
+
+  @Test
+  public void testWriteToFile() {
+    List<List<Pixel>> grid;
+    Stack<Operation> his;
+    BasicImage snail = new BasicImage();
+    File file = new File("res/snail.ppm");
+   checkerboard.writeToFile;
+    System.out.println(file);
+  }
+
+  @Test
+  public void testSaveAs() throws IOException {
+    File f = new File("res/snail.ppm");
+    checkerboard.saveAs("res/snail.ppm");
+
+  }
+
+  @Test
+  public void testApply() {
+    Operation op = new Blur();
+    checkerboard.apply(op);
+    System.out.println(checkerboard);
+  }
+
+  @Test
+  public void testGetPixels() {
+    setup();
+    checkerboard.getPixels();
+    System.out.println(checkerboard.getPixels());
   }
 
 
